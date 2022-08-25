7e37ee0579d52917a808d209d3e5341b3bdd2013
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-08-25 10:14:47 +0530

    Add project files.

---
+++ FileHandlingDemoContd/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FileHandlingDemoContd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Sulakshana\MphasisAugust\";
+            Console.WriteLine("Enter the file name");
+            string fname = Console.ReadLine();
+
+            //File.Create(path+fname);
+            fname = string.Concat(path, fname);
+            //  File.Create(fname);
+            //Console.WriteLine("file created successfully...");
+            //Console.WriteLine("Enter your file contents");
+            //string con=Console.ReadLine();
+            //string[] contents = new string[2];
+            //contents[0] = con;
+            //File.WriteAllLines(fname, contents);
+            //File.WriteAllText(fname, con);
+            //File.AppendAllText(fname, con);
+            //Console.WriteLine("file created successfully with contents...");
+            // File.Copy(fname, "copiedFile.txt");
+            //File.Move(fname, "MovedFile.txt");
+            //Console.WriteLine("Successful....");
+            //bool fileExists=File.Exists(fname);
+            //Console.WriteLine("File Exists=  " + fileExists);
+            //string contents=File.ReadAllText(fname);
+            //Console.WriteLine(contents);
+            //FileInfo fileInfo = new FileInfo(fname);
+            //Console.WriteLine(fileInfo.FullName + "  Full Name");
+            //    Console.WriteLine(fileInfo.DirectoryName + " DirectoryName");
+            //Console.WriteLine(fileInfo.CreationTime);
+            Directory.CreateDirectory("MyDir");
+            
+
+            Console.ReadLine();
+        }
+    }
+}
